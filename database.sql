4acb868565ac566084708f9a7c7435ac9804787d
HariniS1018 <131979231+HariniS1018@users.noreply.github.com>
2023-10-30 15:56:14 +0530

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,7 +1,2 @@
-create table studentsEnrolled(
-enrollId int primary key,
-userId int,
-subjectCode varchar(10),
-	FOREIGN KEY(userId) 
-	  REFERENCES studentsEnrolled(userId)
-);
+select* from attendance;
+
